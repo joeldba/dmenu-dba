28fc0e7385bb327725b3c8e9ff26ca8e3482aa37
joeldba <75224242+joeldba@users.noreply.github.com>
2020-12-13 16:23:29 -0700

    Add files via upload

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#bdae93", "#3c3836" },
+	[SchemeMid] = { "#d8dee9", "#3b4252" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
